95cf2438114223dd7163220b5cfb1c971547df17
Pham Anh <anhapo11@gmail.com>
2016-03-29 14:41:38 +0700

    Initial Commit

---
+++ Exercise 15 - Multi Thread/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Exercise 15 - Multi Thread
+//
+//  Created by Pham Anh on 3/29/16.
+//  Copyright © 2016 com.gaxxanh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Exercise 15 - Multi Thread/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Exercise 15 - Multi Thread
+//
+//  Created by Pham Anh on 3/29/16.
+//  Copyright © 2016 com.gaxxanh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
